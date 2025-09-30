7bec029f3da411f2d3cd447bdd8d9c9393658f1d
NinaOosterlaar <ninaoosterlaar@gmail.com>
2025-09-30 16:53:15 +0200

    oops i do need to also change the batch

--- Batch/collect_yeast_genes.sh
+++ Batch/collect_yeast_genes.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=nina-plots
 #SBATCH --partition=general
 #SBATCH --qos=short
-#SBATCH --time=00:05:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
