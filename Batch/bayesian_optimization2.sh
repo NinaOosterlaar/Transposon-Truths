9a1e3efc3ca12f3db6088e86bd2b9e6e28395092
NinaOosterlaar <ninaoosterlaar@gmail.com>
2026-02-15 12:52:27 +0100

    new run bayesianopt

--- Batch/bayesian_optimization2.sh
+++ Batch/bayesian_optimization2.sh
@@ -34,5 +34,4 @@ srun apptainer exec \
     --random_state 42 \
     --n_jobs 1 \
     --metric "zinb_nll" \
-    --resume_from "AE/results/bayesian_optimization/checkpoints/checkpoint_zinb_nll_20260213_115730.pkl" \
 
