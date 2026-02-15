f42b65592ceaf36ad22a9c603227a55ded5e2baa
NinaOosterlaar <ninaoosterlaar@gmail.com>
2026-02-15 11:07:58 +0100

    continue bayesopt

--- Signal_processing/sliding_mean/sliding_NB_CPD.py
+++ Signal_processing/sliding_mean/sliding_NB_CPD.py
@@ -104,7 +104,7 @@ def save_results(output_folder, dataset_name, change_points, scores, theta_globa
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Apply a sliding window mean change point detection algorithm on discrete count data.")
     parser.add_argument("input_file", type=str, help="Path to the input CSV file containing the count data.")
-    parser.add_argument("--output_folder", type=str, default="Signal_processing/results/sliding_mean_CPD", help="Output folder for results.")
+    parser.add_argument("--output_folder", type=str, default="Signal_processing/results/sliding_mean/sliding_NB_CPD", help="Output folder for results.")
     parser.add_argument("--dataset_name", type=str, default="dataset", help="Name of the dataset being processed.")
     return parser.parse_args()
 
@@ -114,13 +114,12 @@ if __name__ == "__main__":
     input_file = args.input_file
     window_size = [10, 30, 50, 80]
     overlap = 0.5
-    thresholds = np.concatenate([
-        np.linspace(0.5, 2, 8),   # Low thresholds for high recall
-        np.linspace(2, 6, 15),     # Your current range
-        np.linspace(6, 10, 8)      # High thresholds for high precision
-    ])
+    thresholds = np.linspace(0.5, 25, 30)
+    print(thresholds)
     output_folder = args.output_folder
     dataset_name = args.dataset_name
+    # Add dataset name to output folder path
+    output_folder = os.path.join(output_folder, dataset_name)
     
     # Read data
     # datasets = read_csv_file_with_distances(input_file)
