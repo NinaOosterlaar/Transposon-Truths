09bd55fa774a264359fc5e88c96d56434f5da4df
NinaOosterlaar <ninaoosterlaar@gmail.com>
2026-02-20 10:54:14 +0100

    run AE test and got more CPD results

--- Signal_processing/sample_data/more_realistic.py
+++ Signal_processing/sample_data/more_realistic.py
@@ -112,19 +112,19 @@ def make_random_zinb_counts(
 if __name__ == "__main__":
     plot = False
     generate = True
-    number_of_samples = 1000
+    number_of_samples = 1
 
     if generate:
         for i in range(number_of_samples):
             print(f"Generating sample {i}")
             number_of_regions = 2000
-            output_path = f"Data/simulated_data/realistic_data_{i}.csv"
+            output_path = f"Signal_processing/sample_data/noisy_data.csv"
 
             counts, boundaries, params = make_random_zinb_counts(
                 n_regions=number_of_regions,
                 mean_range=(0.1, 40.0),
                 length_mean_range=(40, 1000),
-                theta_range=(5.0, 60.0),
+                theta_range=(0.01, 10.0),
                 zero_prob_range=(0.7, 0.95),
                 min_region_length=20,
                 probability_zero_region=0.1,
@@ -134,7 +134,7 @@ if __name__ == "__main__":
             df = pd.DataFrame({"Position": positions, "Value": counts})
             df.to_csv(output_path, index=False)
             # Save params
-            params_output_path = f"Data/simulated_data/realistic_data_params_{i}.csv"
+            params_output_path = f"Signal_processing/sample_data/noisy_data_params.csv"
             params_df = pd.DataFrame(params)
             params_df.to_csv(params_output_path, index=False)
 
