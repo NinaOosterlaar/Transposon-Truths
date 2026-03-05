431578386770da30937d1d1ed49bd10216f78aca
NinaOosterlaar <ninaoosterlaar@gmail.com>
2026-03-05 10:17:16 +0100

    make loss bad

--- Batch/test_AE.sh
+++ Batch/test_AE.sh
@@ -11,12 +11,12 @@
 #SBATCH --mail-user=n.i.m.oosterlaar@student.tudelft.nl
 #SBATCH --output=slurm_%A_%a.out
 #SBATCH --error=slurm_%A_%a.err
-#SBATCH --array=0-10
+#SBATCH --array=0
 
 set -euo pipefail
 
 NOISE_LEVEL=0.15
-MU_OFFSETs=(0 1 2 3 4 5 6 7 8 9 10)
+MU_OFFSETs=(0)
 MU_OFFSET="${MU_OFFSETs[$SLURM_ARRAY_TASK_ID]}"
 
 echo "Running with noise_level=${NOISE_LEVEL}"
